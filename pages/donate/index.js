72abf8b343d0e8b2f6b3ae459a5cb98f697a6864
YolhaYu <olenka_glushko1993@mail.ru>
2023-02-28 01:21:44 +0300

    feat: add project

---
+++ pages/donate/index.js
@@ -0,0 +1,90 @@
+const burger = document.querySelector('.header-burger');
+const windowClose = document.querySelector('.window-close');
+const navList = document.querySelector('.nav-list');
+const navItem = document.querySelectorAll('.nav-item_burger');
+const nav = document.querySelector('.nav_burger');
+
+burger.addEventListener('click', (e) => {
+    nav.classList.add('nav_burger-active');
+});
+windowClose.addEventListener('click', (e) => {
+    nav.classList.remove('nav_burger-active');
+});
+for(let i = 0; i < navItem.length; i++) {
+    navItem[i].addEventListener('click', () => {
+        nav.classList.remove('nav_burger-active');
+    });
+}
+document.addEventListener('click', event => {
+    let e = nav;
+    let burg = burger;
+    if (!e.contains(event.target) && !burg.contains(event.target)) {
+        nav.classList.remove('nav_burger-active');
+    };
+  });
+
+//   Pick and feed a friend 
+
+const form = document.querySelector('.progress-bar_form');
+const inputs = document.querySelectorAll('.progress-bar_radio');
+const priceList = document.querySelector('.prise-list');
+const priceItem = document.querySelectorAll('.prise-item');
+const anotherAmount = document.getElementById('number');
+const label = document.querySelectorAll('.label');
+
+console.log(priceItem);
+
+function limit() {
+    let val = anotherAmount.value;
+    // console.log(val);
+    if (val.length >= 5){
+        val = val.slice (0,4);
+    };
+}
+
+for(let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('input', (e) => {
+        inputs[i].checked = true;
+        label.forEach(item => item.classList.remove('label_checked'))
+        label[i].classList.add('label_checked');
+
+        priceItem.forEach(item => item.classList.remove('prise-item_active'));
+        priceItem[i].classList.add('prise-item_active');
+
+        let newPrice = inputs[i].value;
+        anotherAmount.setAttribute('value', newPrice);
+        console.log(anotherAmount);
+    });
+}
+anotherAmount.addEventListener('input', function() {
+    let val = anotherAmount.value;   
+    for (let i = 0; i < priceItem.length; i++) {
+        let price = priceItem[i].textContent;
+         
+        if(price.includes(val)) {            
+            priceItem.forEach(item => item.classList.remove('prise-item_active'));
+            priceItem[i].classList.add('prise-item_active');  
+            label.forEach(item => item.classList.remove('label_checked'));
+            label[i].classList.add('label_checked');         
+        }else {
+            priceItem[i].classList.remove('prise-item_active');
+            label[i].classList.remove('label_checked'); 
+        }
+        if(!val) {
+            console.log('null');
+            priceItem.forEach(item => item.classList.remove('prise-item_active'));
+            label.forEach(item => item.classList.remove('label_checked'));
+        } 
+    }
+    this.value = this.value.substr(0, 4);
+});
+function media() {
+    if (document.documentElement.clientWidth < 855) {
+        priceItem[5].classList.add('prise-item_active'); 
+        label[5].classList.add('label_checked');
+        anotherAmount.setAttribute('value', 100);
+    }
+}
+media();
+
+
---
+++ pages/main/index.js
@@ -0,0 +1,141 @@
+const burger = document.querySelector('.header-burger');
+const windowClose = document.querySelector('.window-close');
+const navList = document.querySelector('.nav-list');
+const navItem = document.querySelectorAll('.nav-item_burger');
+const nav = document.querySelector('.nav_burger');
+
+burger.addEventListener('click', (e) => {
+    nav.classList.add('nav_burger-active');
+});
+windowClose.addEventListener('click', (e) => {
+    nav.classList.remove('nav_burger-active');
+});
+for(let i = 0; i < navItem.length; i++) {
+    navItem[i].addEventListener('click', () => {
+        nav.classList.remove('nav_burger-active');
+    });
+}
+document.addEventListener('click', event => {
+    let e = nav;
+    let burg = burger;
+    if (!e.contains(event.target) && !burg.contains(event.target)) {
+        nav.classList.remove('nav_burger-active');
+    };
+  });
+
+//   карусель Pets
+
+const wrapCards = document.querySelector('.wrap-cards');
+let card = document.querySelectorAll('.card');
+const arrowNext = document.querySelector('.arrow-next');
+const arrowPrev = document.querySelector('.arrow-prev');
+let isEnabled = true;
+let currentItem = 0;
+
+const shuffle = (deck) => { 
+    for (let i = deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = deck[i];
+      deck[i] = deck[j];
+      deck[j] = temp;
+    }
+    return deck;
+};
+function creatCard() {
+    let frag = document.createDocumentFragment();
+    let a = shuffle(wrapCards.children);
+    console.log(a);
+    frag.appendChild(a[0]);
+    wrapCards.appendChild(frag);
+}
+function changeCurrentItem(n) {
+    currentItem = (n + wrapCards.length) % wrapCards.length;
+}
+function hideCards(direction) {
+    isEnabled = false;
+    wrapCards.classList.add(direction);
+    wrapCards.addEventListener('animationend', function() {
+        this.classList.remove(direction);
+    });
+}
+function showCards(direction) {
+    wrapCards.classList.add(direction);
+    wrapCards.addEventListener('animationend', function() {
+        this.classList.remove(direction);
+        isEnabled = true;
+    });
+}
+function prevItem(n) {
+    hideCards('to-right');
+    changeCurrentItem(currentItem - 1);
+    showCards('from-left');
+
+}
+function nextItem(n) {
+    hideCards('to-left');
+    changeCurrentItem(currentItem + 1);
+    showCards('from-right');
+}
+arrowNext.addEventListener('click', () => {
+    if(isEnabled) {
+        nextItem(currentItem);
+        creatCard();
+    }    
+});
+arrowPrev.addEventListener('click', () => {
+    if(isEnabled) {
+        prevItem(currentItem);
+        creatCard();
+    }    
+})
+
+// Карусель в блоке Testimonials
+
+const wrapTestimonials = document.querySelector('.wrap-testimonials');
+const testimonials = document.querySelectorAll('.testimonial');
+const scrolRange = document.querySelector('.scrol-range');
+// console.log(wrapTestimonials);
+
+function showNext() {
+    wrapTestimonials.classList.add('wrap-testimonial-next');
+}
+
+function showPrev() {
+    wrapTestimonials.classList.add('wrap-testimonial-prev');
+}
+
+
+scrolRange.addEventListener('input', () => { 
+    let value = scrolRange.value;
+    let step = scrolRange.step;
+    console.log(step);
+    console.log(value);
+    if (value > 0) {
+        console.log(value === (value + step));
+        showNext();
+    }else {
+        showPrev();
+    }
+})
+
+// pop-up
+
+const popUp = document.querySelector('.pop-up');
+const popUpContent = document.querySelector('.pop-up-content');
+const popUpClose = document.querySelector('.pop-up-close');
+const popUpOverlay = document.querySelector('.pop-up_overlay');
+
+for (let i = 0; i < testimonials.length; i++) {
+    testimonials[i].addEventListener('click', () => {
+        popUp.classList.add('pop-up-active');
+    });
+}
+popUpClose.addEventListener('click', () => {
+    popUp.classList.remove('pop-up-active');
+});
+
+popUp.addEventListener('click', () => {
+    popUp.classList.remove('pop-up-active');
+});
+
+
